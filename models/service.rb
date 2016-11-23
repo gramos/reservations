c4cd1a74e01e993945907ff5f5896e9e6c540a4c
Gaston Ramos <ramos.gaston@gmail.com>
2016-11-23 16:16:02 -0300

    Little refactor.

--- models/service.rb
+++ models/service.rb
@@ -2,15 +2,11 @@ class Service < Sequel::Model
   many_to_one :driver
   many_to_one :scheduled_time
   one_to_many :reservations
-  
+
   def available_seats
     type = DB[:reservation_types].where(:name => 'Comun').first
 
-    human_reservations = reservations.select do |r|
-                           r[:type_id] == type[:id]
-                         end 
-
-    reserved_seats = human_reservations.
+    reserved_seats = reservations.select{|r| r[:type_id] == type[:id] }.
                      reduce(0){ |sum, r| sum = sum + r.quantity }
 
     driver.car_seats - reserved_seats
