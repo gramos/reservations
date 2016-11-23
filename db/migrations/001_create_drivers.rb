4406e6b1b32a6a092ed554d460fca547ad6c00f5
Gaston Ramos <ramos.gaston@gmail.com>
2016-11-23 09:48:22 -0300

    Add field car_seats

--- db/migrations/001_create_drivers.rb
+++ db/migrations/001_create_drivers.rb
@@ -2,13 +2,14 @@ Sequel.migration do
   up do
     create_table(:drivers) do
       primary_key :id
-      String :first_name,  :null => false
-      String :last_name,   :null => false
-      String :dni,         :null => false
-      String :mobile,      :null => true
-      String :car,         :null => true
-      String :car_color,   :null => true
-      String :car_license, :null => true
+      String  :first_name,  :null => false
+      String  :last_name,   :null => false
+      String  :dni,         :null => false
+      String  :mobile,      :null => true
+      String  :car,         :null => true
+      Integer :car_seats,   :null => false
+      String  :car_color,   :null => true
+      String  :car_license, :null => true
     end
   end
 
