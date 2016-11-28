e234ed918a65d6ee9a6ef5adb3a888e61b0c615a
Gaston Ramos <ramos.gaston@gmail.com>
2016-11-28 20:36:53 -0300

    Set env test in integration test helper/
    Add DBSeed::delete_all!

--- lib/dbseed/base.rb
+++ lib/dbseed/base.rb
@@ -7,7 +7,15 @@ module DBSeed
       DBSeed::ScheduledTimes.run!
       DBSeed::Services.run!
       DBSeed::Cities.run!
-    end  
+    end
+
+    def self.delete_all!
+      [:reservations, :services, :reservation_types,
+       :scheduled_times, :customers, :addresses, :cities,
+       :drivers].each do |x|
+        Sequel::Model.db.from(x).delete
+      end
+    end
   end
 
 end
--- test/integration/integration_helper.rb
+++ test/integration/integration_helper.rb
@@ -1,8 +1,12 @@
 require "cuba/capybara"
+
+ENV['ENV'] = 'test'
+
 require_relative "../../app"
 
 Dir["./db/seeds/*.rb"].each { |rb| require rb }
 
 prepare do
+  DBSeed::Base.delete_all!
   DBSeed::Base.run_all!
 end
