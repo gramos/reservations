f5fabe1400ed8d25fdf4a28101aa52e968b6acaf
Gaston Ramos <ramos.gaston@gmail.com>
2016-11-29 10:02:38 -0300

    First working integration test.

--- test/integration/reservations_test.rb
+++ test/integration/reservations_test.rb
@@ -2,7 +2,6 @@ require_relative "integration_helper"
 
 
 scope 'Daily Services' do
-
   setup do
     @service = Service.where(:date => Date.today).first
   end
@@ -14,14 +13,22 @@ scope 'Daily Services' do
 
   test_js 'Hago una reserva de un servicio' do
     visit '/'
-    click_link "button-create-reservation-#{@service.id}"
 
+    within "#servicio_#{@service.id}" do
+      page.has_content?('( 4 )')
+    end
+
+    click_link "button-create-reservation-#{@service.id}"
     fill_in 'reservation[customer][last_name]', with: 'Lee'
     fill_in 'reservation[customer][first_name]', with: 'Bruce'
 
     fill_in 'reservation[address][street]', with: 'Hollywood Boulevard'
     fill_in 'reservation[address][number]', with: '1234'
     select 'Comun', from: 'reservation[reservation][type_id]'
+    click_button 'Crear Reserva'
+
+    within "#servicio_#{@service.id}" do
+      page.has_content?('( 3 )')
+    end
   end
-  
 end
