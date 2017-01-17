0cff71c7af1a7dfb103912c6e9cec503bf5fee6f
Gaston Ramos <ramos.gaston@gmail.com>
2017-01-17 10:50:43 -0300

    Add right scheduled times.

--- db/seeds/drivers.rb
+++ db/seeds/drivers.rb
@@ -5,60 +5,59 @@ module DBSeed
     def self.run!
       DB[:drivers].insert(:first_name => 'Nestor',
                           :last_name  => 'Farina',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
+                          :dni        => '13958149',
+                          :mobile     => '3416688834',
+                          :car        => 'Volkswagen Suran 2013',
+                          :car_color  => 'Gris Oscuro',
+                          :car_license=> 'MKG-262', :car_seats => 4)
 
       DB[:drivers].insert(:first_name => 'Marcelo',
                           :last_name  => 'Bissuti',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
+                          :dni        => '10594450',
+                          :mobile     => '3412011883',
+                          :car        => 'Renault Fluence 2015',
+                          :car_color  => 'Gris claro',
+                          :car_license=> 'PCT-868', :car_seats => 4)
 
       DB[:drivers].insert(:first_name => 'Andres',
                           :last_name  => 'Ricci',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
+                          :dni        => '25101854',
+                          :mobile     => '3416505478',
+                          :car        => 'Ford Eco Sport 2014',
+                          :car_color  => 'Negro',
+                          :car_license=> 'MCN-982', :car_seats => 4)
 
       DB[:drivers].insert(:first_name => 'Ariel',
                           :last_name  => 'Aguilar',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
+                          :dni        => '25101854',
+                          :mobile     => '3416505478',
+                          :car        => 'Chevrolet Cobalt 2014',
+                          :car_color  => 'Azul',
+                          :car_license=> 'NNQ-946', :car_seats => 4)
 
       DB[:drivers].insert(:first_name => 'Gastón',
                           :last_name  => 'Petinari',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
+                          :dni        => '29583007',
+                          :mobile     => '3413822085',
+                          :car        => 'Chevrolet Meriva 2006',
+                          :car_color  => 'Azul oscuro',
+                          :car_license=> 'ETC-650', :car_seats => 4)
 
-      DB[:drivers].insert(:first_name => 'Fredy',
-                          :last_name  => 'Fredy',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
+      DB[:drivers].insert(:first_name => 'Gustavo Ariel',
+                          :last_name  => 'Yarros',
+                          :dni        => '26719728',
+                          :mobile     => '3416922700',
+                          :car        => 'Citroen C4 Lounge 2016',
+                          :car_color  => 'Blanco Nacarado',
+                          :car_license=> 'AA487BS', :car_seats => 4)
 
       DB[:drivers].insert(:first_name => 'Juan Carlos',
-                          :last_name  => 'Carlos',
-                          :dni        => '123456',
-                          :mobile     => '',
-                          :car        => '',
-                          :car_color  => '',
-                          :car_license=> '', :car_seats => 4)
-
+                          :last_name  => 'Vetrone',
+                          :dni        => '6062371',
+                          :mobile     => '3415020494',
+                          :car        => 'Renault Fluence 2015',
+                          :car_color  => 'Blanco',
+                          :car_license=> 'MGO-104', :car_seats => 4)
     end
   end
 end
--- db/seeds/scheduled_times.rb
+++ db/seeds/scheduled_times.rb
@@ -1,11 +1,10 @@
 module DBSeed
   class ScheduledTimes
     def self.run!
-      st = [
-        {:time => '06:00'},
-        {:time => '07:00'},
+      st_rosario_lunes_a_viernes = [
+        {:time => '05:30'},
+        {:time => '06:30'},
         {:time => '08:00'},
-        {:time => '09:00'},
         {:time => '10:00'},
         {:time => '11:00'},
         {:time => '12:00'},
@@ -16,23 +15,129 @@ module DBSeed
         {:time => '17:00'},
         {:time => '18:00'},
         {:time => '19:00'},
-        {:time => '20:00'},
-        {:time => '21:00'},
-        {:time => '22:00'}
+        {:time => '20:00'}
       ]
 
-      [0, 1, 2, 3, 4, 5, 6].each do |d|
-        st.each do |t|
+      [1, 2, 3, 4, 5].each do |d|
+        st_rosario_lunes_a_viernes.each do |t|
           city = DB[:cities].where(:name => /rosario/i).first
           data = t.merge( :day => d, :city_id => city[:id] )
           DB[:scheduled_times].insert data
+        end
+      end
 
+      st_san_nicolas_lunes_a_viernes = [
+        {:time => '06:00'},
+        {:time => '07:00'},
+        {:time => '08:00'},
+        {:time => '09:00'},
+        {:time => '10:00'},
+        {:time => '12:00'},
+        {:time => '13:00'},
+        {:time => '14:00'},
+        {:time => '15:00'},
+        {:time => '16:00'},
+        {:time => '17:00'},
+        {:time => '18:00'},
+        {:time => '19:00'},
+        {:time => '20:00'}
+      ]
+
+      [1, 2, 3, 4, 5].each do |d|
+        st_san_nicolas_lunes_a_viernes.each do |t|
           city = DB[:cities].where(:name => /san nicolas/i).first
           data = t.merge( :day => d, :city_id => city[:id] )
           DB[:scheduled_times].insert data
         end
-
       end
+
+       st_rosario_sabado = [
+        {:time => '06:00'},
+        {:time => '08:00'},
+        {:time => '10:00'},
+        {:time => '11:00'},
+        {:time => '12:00'},
+        {:time => '13:00'},
+        {:time => '14:00'},
+        {:time => '15:00'},
+        {:time => '16:00'},
+        {:time => '17:00'},
+        {:time => '18:00'},
+        {:time => '19:00'},
+        {:time => '20:00'}
+      ]
+
+       st_rosario_sabado.each do |t|
+         city = DB[:cities].where(:name => /rosario/i).first
+         data = t.merge( :day => 6, :city_id => city[:id] )
+         DB[:scheduled_times].insert data
+       end
+
+       st_san_nicolas_sabado = [
+         {:time => '06:00'},
+         {:time => '08:00'},
+         {:time => '10:00'},
+         {:time => '12:00'},
+         {:time => '13:00'},
+         {:time => '14:00'},
+         {:time => '15:00'},
+         {:time => '16:00'},
+         {:time => '17:00'},
+         {:time => '18:00'},
+         {:time => '19:00'},
+         {:time => '20:00'}
+      ]
+
+       st_san_nicolas_sabado.each do |t|
+          city = DB[:cities].where(:name => /san nicolas/i).first
+          data = t.merge( :day => 6, :city_id => city[:id] )
+          DB[:scheduled_times].insert data
+        end
+
+       st_rosario_domingo = [
+         {:time => '06:30'},
+         {:time => '08:00'},
+         {:time => '10:00'},
+         {:time => '11:00'},
+         {:time => '12:00'},
+         {:time => '13:00'},
+         {:time => '14:00'},
+         {:time => '15:00'},
+         {:time => '16:00'},
+         {:time => '17:00'},
+         {:time => '18:00'},
+         {:time => '19:00'},
+         {:time => '20:00'},
+         {:time => '20:00'}
+      ]
+
+       st_rosario_domingo.each do |t|
+          city = DB[:cities].where(:name => /rosario/i).first
+          data = t.merge( :day => 0, :city_id => city[:id] )
+          DB[:scheduled_times].insert data
+       end
+
+        st_san_nicolas_domingo = [
+         {:time => '08:00'},
+         {:time => '10:00'},
+         {:time => '12:00'},
+         {:time => '13:00'},
+         {:time => '14:00'},
+         {:time => '15:00'},
+         {:time => '16:00'},
+         {:time => '17:00'},
+         {:time => '18:00'},
+         {:time => '18:00'},
+         {:time => '19:00'},
+         {:time => '20:00'},
+         {:time => '22:00'}
+      ]
+
+       st_san_nicolas_domingo.each do |t|
+          city = DB[:cities].where(:name => /san nicolas/i).first
+          data = t.merge( :day => 0, :city_id => city[:id] )
+          DB[:scheduled_times].insert data
+        end
     end
   end
 end
