3da4bb7a07d3664a6de7368dd6e44287e4da70b1
Gaston Ramos <ramos.gaston@gmail.com>
2017-02-03 18:13:06 -0300

    Added some default users

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -6,10 +6,20 @@ module DBSeed
                         :username => 'lacasa',
                         :password => '12345678')
 
-      User.create(:first_name => 'Gustavo',
+      User.create(:first_name => 'Gustavo Ariel',
                         :last_name => 'Yarros',
-                        :username => 'gustavo',
-                        :password => '12345678')
+                        :username => 'gyarros',
+                        :password => 'gyarros.26719728')
+
+      User.create(:first_name => 'Marina Denise',
+                        :last_name => 'Arenas',
+                        :username => 'marenas',
+                        :password => 'marenas.36008581')
+
+      User.create(:first_name => 'Jesica',
+                        :last_name => 'Garcia',
+                        :username => 'jgarcia',
+                        :password => 'jgarcia.27674287')
 
     end
   end
